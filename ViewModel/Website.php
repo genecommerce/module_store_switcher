99202051e647a1a7a09dbbd4b4cc2318d582811b
Josh Carter <josh.carter@gene.co.uk>
2022-07-21 17:50:04 +0100

    v0.0.2 - Add Country code to store data and add new getCurrentStoreCountry method

--- ViewModel/Website.php
+++ ViewModel/Website.php
@@ -3,41 +3,58 @@ declare(strict_types=1);
 
 namespace Gene\StoreSwitcher\ViewModel;
 
+use Magento\Directory\Helper\Data as DirectoryHelper;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManagerInterface;
 
 class Website implements ArgumentInterface
 {
-    /** @var StoreManagerInterface */
-    protected $storeManager;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
 
     /**
-     * Website constructor.
+     * @var DirectoryHelper
+     */
+    private $directoryHelper;
+
+    /**
+     * Website constructor
+     *
+     * @param DirectoryHelper $directoryHelper
      * @param StoreManagerInterface $storeManager
      */
     public function __construct(
+        DirectoryHelper $directoryHelper,
         StoreManagerInterface $storeManager
-    )
-    {
+    ) {
+        $this->directoryHelper = $directoryHelper;
         $this->storeManager = $storeManager;
     }
 
     /**
      * @return array<int,array<string, mixed>>
      */
-    public function getAll()
+    public function getAll(): array
     {
         $storeList = [];
         $stores = $this->storeManager->getStores();
-        /** @var StoreInterface $store */
+        /** @var StoreInterface|Store $store */
         foreach ($stores as $store) {
+            $countryCode = $this->directoryHelper->getDefaultCountry($store);
+            $countryCode = $countryCode === 'GB' ?
+                'UK' :
+                $countryCode;
             $storeId = $store->getId();
             $storeList[$storeId]['id'] = $storeId;
             $storeList[$storeId]['name'] = $store->getName();
             $storeList[$storeId]['currency'] = $store->getCurrentCurrency()->getCurrencySymbol(); /** @phpstan-ignore-line */
             $storeList[$storeId]['code'] = $store->getCode();
+            $storeList[$storeId]['country_code'] = $countryCode;
         }
         return $storeList;
     }
@@ -46,10 +63,17 @@ class Website implements ArgumentInterface
      * @return StoreInterface
      * @throws NoSuchEntityException
      */
-    public function getCurrent()
+    public function getCurrent(): StoreInterface
     {
-        /** @var StoreInterface $store */
-        $store = $this->storeManager->getStore();
-        return $store;
+        return $this->storeManager->getStore();
+    }
+
+    public function getCurrentStoreCountry(): string
+    {
+        $store = $this->getCurrent();
+        $country = $this->directoryHelper->getDefaultCountry($store);
+        return $country === 'GB' ?
+            'UK' :
+            $country;
     }
 }
